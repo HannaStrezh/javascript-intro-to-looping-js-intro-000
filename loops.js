474f8b9382c086e1a1494a5afb27349332161615
Hanna Strezh <hannastrezh@gmail.com>
2018-07-13 23:08:47 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,8 +1,9 @@
 function forLoop(array) {
-      for (let i=0; i<25; i++)
-      {if(i===1){
-        array.push("I am 1 strange loop.");
-      }
+      for (let i=0; i<25; i++){
+        if(i===1){
+          array.push("I am 1 strange loop.");
+          
+        }else{
         
       }
       console.log(array);
